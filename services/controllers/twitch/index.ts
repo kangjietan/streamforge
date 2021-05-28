613ab4c07489f8304028fe3cdc45c26a84b391d0
kangjietan <kangjietan@gmail.com>
2021-05-27 22:58:14 -0700

    Get clips from user controller.

--- services/controllers/twitch/index.ts
+++ services/controllers/twitch/index.ts
@@ -28,3 +28,29 @@ export async function getStreams(req: Request, res: Response) {
       console.log(error);
     });
 }
+
+export async function getClipsFromUser(req: Request, res: Response) {
+  const { broadcasterID } = req.query;
+
+  const url = `https://api.twitch.tv/helix/clips?broadcaster_id=${broadcasterID}`;
+
+  const options: AxiosRequestConfig = {
+    url,
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${process.env.APP_ACCESS_TOKEN}`,
+      "Client-ID": process.env.TWITCH_CLIENT_ID,
+      Accept: "application/vnd.twitchtv.v5+json",
+    },
+  };
+
+  return axios(options)
+    .then((response) => {
+      console.log(response);
+      console.log(response.data);
+      res.json(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
