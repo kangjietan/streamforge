2091518f79b9115388ce682277414131a1c06211
kangjietan <kangjietan@gmail.com>
2021-05-29 14:02:32 -0700

    Variables.

--- client/src/themes/GlobalStyles.ts
+++ client/src/themes/GlobalStyles.ts
@@ -1,13 +1,20 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
+  :root {
+    --container-bg-color: hsla(0,0%, 100%, .05);
+    --container-border-radius: 12px;
+    --primary-text-color: #fff;
+  }
+
   body {
     padding-bottom: 96px;
     background-color: #12141d;
-    font-family: Inter,sans-serif;
-    color: hsla(0,0%,100%,.5);
+    font-family: Inter, sans-serif;
+    color: hsla(0,0%, 100%, .5);
     font-size: 16px;
     font-weight: 500;
+    line-height: 24px;
   }
 `;
 
