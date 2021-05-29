50f187016c149ba31960976f5f5eb651ed02c545
kangjietan <kangjietan@gmail.com>
2021-05-29 14:03:08 -0700

    Fetch clips from stream.

--- client/src/components/Clip/Clip.tsx
+++ client/src/components/Clip/Clip.tsx
@@ -1,28 +1,62 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-import { IClipsFromStreamsData, IClip } from "../App";
-import { Container, EmbedClip, EmbedClipContainer } from "./styles";
+import { IClipsFromStreamsData, IClip, IStream } from "../App";
+import {
+  Container,
+  CurrentStreamer,
+  EmbedClip,
+  EmbedClipContainer,
+  GoBackClip,
+  SkipClip,
+} from "./styles";
+
+import { shuffleArray } from "../../utils";
+
+const serverUrl = "http://localhost:3000";
 
 interface Props {
-  streamClips: IClipsFromStreamsData;
+  // streamClips: IClipsFromStreamsData;
+  stream: IStream;
 }
 
 const Clip: React.FunctionComponent<Props> = (props) => {
   const [currentRandomClip, setCurrentRandomClip] = useState<IClip>();
+  const [streamClips, setStreamClips] = useState<IClipsFromStreamsData>();
+
+  const fetchClipsFromStreams = async (stream?: IStream) => {
+    if (stream) {
+      const url = `${serverUrl}/twitch/clips?broadcasterID=${stream.user_id}&gameID=${stream.game_id}`;
+      const responseData: IClipsFromStreamsData = await axios.get(url).then((response) => {
+        console.log(response);
+        return response.data;
+      });
+
+      const shuffledClips: IClip[] = shuffleArray(responseData.data);
+
+      setStreamClips(responseData);
+      setCurrentRandomClip(shuffledClips[0]);
+    }
+  };
 
   useEffect(() => {
-    if (props.streamClips) {
-      setCurrentRandomClip(props.streamClips.data[0]);
+    if (props.stream) {
+      fetchClipsFromStreams(props.stream);
     }
-  }, [props.streamClips]);
+  }, [props.stream]);
+
+  console.log(props.stream);
 
   return (
     <Container>
+      <CurrentStreamer>{currentRandomClip?.broadcaster_name}</CurrentStreamer>
       <EmbedClipContainer>
+        <GoBackClip>Go Back</GoBackClip>
         <EmbedClip
           src={currentRandomClip?.embed_url + "&parent=localhost"}
           allowFullScreen={true}
         ></EmbedClip>
+        <SkipClip>Next Clip</SkipClip>
       </EmbedClipContainer>
     </Container>
   );
