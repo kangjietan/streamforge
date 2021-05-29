bd0e43d9e016d9967e8aebff7acd6f947bc481a7
kangjietan <kangjietan@gmail.com>
2021-05-28 20:58:52 -0700

    Global styles for app.

---
+++ client/src/themes/GlobalStyles.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    padding-bottom: 96px;
+    background-color: #12141d;
+    font-family: Inter,sans-serif;
+    color: hsla(0,0%,100%,.5);
+    font-size: 16px;
+    font-weight: 500;
+  }
+`;
+
+export default GlobalStyles;
