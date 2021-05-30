f54e589eb6e316182c694d44d6506019983ead11
kangjietan <kangjietan@gmail.com>
2021-05-29 23:41:56 -0700

    Render no clips container when the streamer is currently streaming the game. Refactor some code to a Stream component.

--- client/src/components/Clip/Clip.tsx
+++ client/src/components/Clip/Clip.tsx
@@ -4,17 +4,15 @@ import axios from "axios";
 import { IClipsFromStreamsData, IClip, IStream } from "../App";
 import {
   Container,
-  CurrentStreamer,
   EmbedClip,
   EmbedClipContainer,
   GoBackClip,
-  GoBackStream,
-  GoToStreamButton,
   SkipClip,
-  SkipStream,
-  StreamContainer,
-  StreamInformationContainer,
+  NoClipsContainer,
+  ViewOtherClipsButton,
 } from "./styles";
+
+import Stream from "../Stream/Stream";
 
 import renderSVG from "../../svgs/renderSVG";
 
@@ -25,16 +23,20 @@ const serverUrl = "http://localhost:3000";
 interface Props {
   stream: IStream;
   handleStreamOffset: (offset: "BACK" | "NEXT") => void;
+  streamOffset: number;
+  fetchMoreStreams: boolean;
 }
 
 const Clip: React.FunctionComponent<Props> = (props) => {
-  const { stream, handleStreamOffset } = props;
+  const { stream, handleStreamOffset, streamOffset, fetchMoreStreams } = props;
 
   const [currentRandomClip, setCurrentRandomClip] =
-    useState<IClip | null>(null);
-  const [streamClips, setStreamClips] = useState<IClipsFromStreamsData>();
-  const [filteredClips, setFilteredClips] = useState<IClip[]>([]);
-  const [clipOffset, setClipOffset] = useState(0);
+    useState<IClip | null>(null); // Current game streamed related clip
+  const [streamClips, setStreamClips] =
+    useState<IClipsFromStreamsData | null>(null); // Top clips from stream
+  const [filteredClips, setFilteredClips] = useState<IClip[]>([]); // Clips that are currently related to the current streamed game
+  const [clipOffset, setClipOffset] = useState(0); // Navigate around clips
+  const [noGameClips, setNoGameClips] = useState(false); // If the clips do not pertain to the currently streamed game
 
   const fetchClipsFromStreams = async (stream?: IStream) => {
     if (stream) {
@@ -42,7 +44,6 @@ const Clip: React.FunctionComponent<Props> = (props) => {
       const responseData: IClipsFromStreamsData = await axios
         .get(url)
         .then((response) => {
-          console.log(response);
           return response.data;
         });
 
@@ -56,104 +57,127 @@ const Clip: React.FunctionComponent<Props> = (props) => {
       // If streamer is playing a game that is not in the top clips
       // Move on to next stream
       if (filteredByGameClips.length === 0) {
-        handleStreamOffset("NEXT");
-      } else {
+        setNoGameClips(true);
         setStreamClips(responseData);
-        setCurrentRandomClip(filteredByGameClips[clipOffset]);
+        setClipOffset(0);
+      } else {
+        setNoGameClips(false);
+        setClipOffset(0);
+        setStreamClips(responseData);
         setFilteredClips(filteredByGameClips);
+        setCurrentRandomClip(filteredByGameClips[clipOffset]);
       }
     }
   };
 
+  const clearData = () => {
+    setCurrentRandomClip(null);
+    setFilteredClips([]);
+    setStreamClips(null);
+    setNoGameClips(false);
+    setClipOffset(0);
+  };
+
+  const streamID = stream ? stream.id : 0;
+
   useEffect(() => {
     // If stream exists
-    if (props.stream) {
-      fetchClipsFromStreams(props.stream);
+    if (stream) {
+      fetchClipsFromStreams(stream);
     }
-  }, [props.stream]);
+  }, [streamID]);
+
+  console.log(stream);
 
   return (
     <Container>
-      <StreamContainer>
-        <GoBackStream
-          onClick={() => {
-            setCurrentRandomClip(null);
-            handleStreamOffset("BACK");
-          }}
-        >
-          Go Back
-        </GoBackStream>
-        <StreamInformationContainer>
-          <CurrentStreamer>
-            {currentRandomClip?.broadcaster_name}
-          </CurrentStreamer>
-          <GoToStreamButton
-            href={stream ? `https://www.twitch.tv/${stream.user_login}` : ""}
-            target="_blank"
+        <Stream
+          streamOffset={streamOffset}
+          clearData={clearData}
+          currentRandomClip={currentRandomClip}
+          handleStreamOffset={handleStreamOffset}
+          stream={stream}
+        />
+      {/* Streamer is not currently streaming game so don't display */}
+      {!noGameClips ? (
+        <EmbedClipContainer>
+          <GoBackClip
+            // Render after offset > 0
+            style={{
+              visibility: clipOffset > 0 ? "visible" : "hidden",
+              pointerEvents: clipOffset > 0 ? "all" : "none",
+            }}
+            onClick={() => {
+              const nextOffset = clipOffset - 1;
+
+              // Decrease offset if there are clips to be retrieved
+              if (nextOffset >= 0) {
+                setClipOffset(nextOffset);
+              } else {
+                return;
+              }
+
+              // If there are filtered clips
+              if (filteredClips) {
+                setCurrentRandomClip(filteredClips[nextOffset]);
+              }
+            }}
           >
-            Watch Stream
-          </GoToStreamButton>
-        </StreamInformationContainer>
-        <SkipStream
-          onClick={() => {
-            setCurrentRandomClip(null);
-            handleStreamOffset("NEXT");
-          }}
-        >
-          Next Stream
-        </SkipStream>
-      </StreamContainer>
-      <EmbedClipContainer>
-        <GoBackClip
-          // Render after offset > 0
-          style={{
-            visibility: clipOffset > 0 ? "visible" : "hidden",
-            pointerEvents: clipOffset > 0 ? "all" : "none",
-          }}
-          onClick={() => {
-            const nextOffset = clipOffset - 1;
-
-            // Decrease offset if there are clips to be retrieved
-            if (nextOffset >= 0) {
-              setClipOffset(nextOffset);
-            } else {
-              return;
+            {renderSVG("chevronCircleLeft")}
+          </GoBackClip>
+          <EmbedClip
+            src={
+              currentRandomClip
+                ? currentRandomClip.embed_url + "&parent=localhost"
+                : ""
             }
-
-            // If there are filtered clips
-            if (filteredClips) {
-              setCurrentRandomClip(filteredClips[nextOffset]);
-            }
-          }}
-        >
-          {renderSVG("chevronCircleLeft")}
-        </GoBackClip>
-        <EmbedClip
-          src={
-            currentRandomClip
-              ? currentRandomClip.embed_url + "&parent=localhost"
-              : ""
-          }
-          allowFullScreen={true}
-        ></EmbedClip>
-        <SkipClip
-          onClick={() => {
-            const nextOffset = clipOffset + 1;
-            // Increase offset if there are clips to be retrieved
-            if (nextOffset < filteredClips.length) {
-              setClipOffset(nextOffset);
-            } else {
-              return;
-            }
-            // If there are filtered clips
-            if (filteredClips) {
-              setCurrentRandomClip(filteredClips[nextOffset]);
-            }
-          }}
-        >
-          {renderSVG("chevronCircleRight")}
-        </SkipClip>
-      </EmbedClipContainer>
+            allowFullScreen={true}
+          ></EmbedClip>
+          <SkipClip
+            style={{
+              visibility: !fetchMoreStreams ? "visible" : "hidden",
+              pointerEvents: !fetchMoreStreams ? "all" : "none",
+            }}
+            onClick={() => {
+              const nextOffset = clipOffset + 1;
+              // Increase offset if there are clips to be retrieved
+              if (nextOffset < filteredClips.length) {
+                setClipOffset(nextOffset);
+              } else {
+                return;
+              }
+              // If there are filtered clips
+              if (filteredClips) {
+                setCurrentRandomClip(filteredClips[nextOffset]);
+              }
+            }}
+          >
+            {renderSVG("chevronCircleRight")}
+          </SkipClip>
+          {/* If more streams need to be fetched */}
+        </EmbedClipContainer>
+      ) : (
+        <NoClipsContainer>
+          <p>
+            {`${stream?.user_login} is currently not streaming ${stream?.game_name}`}
+          </p>
+          {/* If there are clips then show them on click */}
+          {streamClips !== null ? (
+            <ViewOtherClipsButton
+              onClick={() => {
+                if (streamClips) {
+                  setFilteredClips(streamClips.data);
+                  setCurrentRandomClip(streamClips.data[0]);
+                  setNoGameClips(false);
+                  setClipOffset(0);
+                }
+              }}
+            >
+              View Other Clips
+            </ViewOtherClipsButton>
+          ) : null}
+        </NoClipsContainer>
+      )}
     </Container>
   );
 };
--- client/src/components/Clip/styles.ts
+++ client/src/components/Clip/styles.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+import {GoToStreamButton} from "../Stream/styles";
+
 export const Container = styled.div`
   display: flex;
   justify-content: center;
@@ -13,62 +15,15 @@ export const Container = styled.div`
   margin-top: 2rem;
 `;
 
-export const StreamContainer = styled.div`
-  display: flex;
-  width: 100%;
-  justify-content: space-evenly;
-  align-items: center;
-`;
-
-export const StreamInformationContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  margin-bottom: 1rem;
-`;
-
-export const CurrentStreamer = styled.h1`
-  color: var(--primary-text-color);
-`;
-
-// export const CurrentGame = styled.h2``;
-
-export const GoToStreamButton = styled.a`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 36px;
-  font-size: 14px;
-  padding-right: 24px;
-  padding-left: 24px;
-  line-height: inherit;
-  text-decoration: none;
-  cursor: pointer;
-  color: var(--primary-text-color);
-  font-weight: 600;
-  border-radius: 6px;
-  box-shadow: inset 0 0 20px 20px transparent;
-  background-color: #6441a4;
-  transition: box-shadow 0.2s ease;
-
-  &:hover {
-    box-shadow: inset 0 0 20px 20px rgba(0, 0, 0, 0.1);
-  }
-`;
-
-export const GoBackStream = styled(GoToStreamButton)`
-  background-color: red;
-`;
-
-export const SkipStream = styled(GoToStreamButton)`
-  background-color: #3c5ccf;
+export const ViewOtherClipsButton = styled(GoToStreamButton)`
+  background-color: green;
 `;
 
 export const EmbedClipContainer = styled.div`
   display: flex;
+  justify-content: center;
   align-items: center;
-  width: calc(100% - 6rem);
+  width: 100%;
   height: 100%;
   margin-bottom: 1rem;
 `;
@@ -106,4 +61,12 @@ export const SkipClip = styled.div`
       color: white;
     }
   }
+`;
+
+export const NoClipsContainer = styled(EmbedClipContainer)`
+  display: flex;
+  font-size: 3rem;
+  line-height: normal;
+  flex-direction: column;
+  text-align: center;
 `;
